915e8ea04a92b923301db7816a72cab4d48792f0
NoraRajeh <139821640+NoraRajeh@users.noreply.github.com>
2023-12-21 01:27:52 +0300

    view client in server

--- server/index.js
+++ server/index.js
@@ -2,17 +2,17 @@ const express = require('express')
 const path = require('path')
 const app = express();
 const PORT = 5000
-// const clientPath = path.resolve(__dirname, '../client/dist')
+const clientPath = path.resolve(__dirname, '../client/dist')
 const db = require('./db')
 app.use(express.json());
-// app.use(express.static(clientPath))
+app.use(express.static(clientPath))
 app.get('/api/recipes', db.getRecipe)
 
 
 //Render Website
-// app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
-// })
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
+})
 
 //Create Links
 app.post('/api/recipes', db.createRecipe)
