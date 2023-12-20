3b7ee8f7614b696535de9ac254e40cbe8ef3d077
NoraRajeh <139821640+NoraRajeh@users.noreply.github.com>
2023-12-21 01:19:37 +0300

    view all Recipe as Card

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+import { Card, Divider } from 'antd';
+
+
+function ItemCard(props) {
+    return (
+        <Card title={props.title} type="inner">
+            <Divider style={{ fontWeight: 'bold' }}>Ingredients</Divider>
+            <p>{props.ingredients}</p>
+            <Divider style={{ fontWeight: 'bold' }}>Directions</Divider>
+            <p>{props.directions}</p>
+        </Card>
+    );
+}
+
+export default ItemCard
